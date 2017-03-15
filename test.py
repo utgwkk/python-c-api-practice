e40ccd578f95994ef1f673df9e719539c2fd3377
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-03-16 08:10:55 +0900

    PEP8

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import random
 
 class MySortTest(unittest.TestCase):
     def test_bubblesort(self):
-        l = [1, 6, 4, 2, 5, 3]
+        alist = [1, 6, 4, 2, 5, 3]
 
         self.assertEqual(
             [1, 2, 3, 4, 5, 6],
@@ -16,7 +16,6 @@ class MySortTest(unittest.TestCase):
             l
         )
 
-
     def test_bubblesort_from_tuple(self):
         tup = (1, 1, 4, 5, 1, 4)
 
@@ -25,9 +24,8 @@ class MySortTest(unittest.TestCase):
             mysort.bubblesort(tup)
         )
 
-
     def test_bubblesort_very_long_list(self):
-        l = list(range(10000))
+        alist = list(range(10000))
         random.shuffle(l)
 
         self.assertEqual(
